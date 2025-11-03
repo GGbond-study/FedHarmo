2c8270d44fa627d3f87378ada1c36d1b2355a2ff
GGbond-study <2352768712@qq.com>
2025-11-03 18:08:08 +0800

    Update run_polyp.sh

--- run_polyp.sh
+++ run_polyp.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python main_seg_al.py --dataset Polyp --fl_method FedHarmo --deterministic True --seed 3 --max_round 200 --gamma 0.99 --annealing_step 10
+CUDA_VISIBLE_DEVICES=0 python main_seg_al.py --dataset Polyp --fl_method FedHarmo --deterministic True --seed 2025 --max_round 200 --gamma 0.99 --annealing_step 10
